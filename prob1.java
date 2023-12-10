154aff4dc6966825c27bb9acf7a8de82f5d81648
Yash Handa <yashanda@amazon.com>
2023-12-10 15:14:00 +0530

    first commit

---
+++ prob1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+public class prob1{
+  //  public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static Scanner scn = new Scanner(System.in);
+    public static void main(String args[]){
+        //int value= Integer.parseInt(br.readLine());
+        int value = scn.nextInt();
+        System.out.println("this is java 1st file"+ value);
+    }
+}
