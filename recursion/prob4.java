227318611ca7fddb92b70fa22453bd985e3824f0
Yash Handa <yashanda@amazon.com>
2023-12-16 19:35:18 +0530

    sudoku

--- recursion/prob4.java
+++ recursion/prob4.java
@@ -180,7 +180,6 @@ public class prob4{
                     d[row-col+arr.length-1]=0;
                     ad[row+col]=0;
                 }
-                
         }
         count+=nQueenShadowBetterSubsequenceBetter(arr, queens, ans, indexRow+1, r, c, d, ad); //queen is not placed.
         return count;
@@ -190,19 +189,94 @@ public class prob4{
         return true;
         return false;
     }
+    public static int[][] constructSudoku(){
+        int[][]arr={{5,3,0,0,7,0,0,0,0},
+                    {6,0,0,1,9,5,0,0,0},
+                    {0,9,8,0,0,0,0,6,0},
+                    {8,0,0,0,6,0,0,0,3},
+                    {4,0,0,8,0,3,0,0,1},
+                    {7,0,0,0,2,0,0,0,6},
+                    {0,6,0,0,0,0,2,8,0},
+                    {0,0,0,4,1,9,0,0,5},
+                    {0,0,0,0,8,0,0,7,9}};
+        
+        return arr;
+    }
+
+    public static boolean sudoku(int [][]arr, int index, int[] r, int[] c, int[] box, int nos){
+        //not 100% accurate by logic, but must be a small thing, check if time permits!
+        if(nos==0){
+            printArr(arr);
+            return true;
+        }
+        if(index==arr.length * arr[0].length){
+            return false;
+        }
+
+        boolean call= false;
+
+        // for(int i=sr;i<arr[0].length;i++){
+        //     for(int j=sc;j<arr.length;j++){
+                int i=index/9,j=index%9;
+                for(int num=1;num<10;num++){
+                    if(arr[i][j]!=0)
+                    call= call || sudoku(arr, index+1, r, c, box, nos);
+                    if(sudokuSafeToPlace(arr, num, i, j, r, c, box)){
+                        arr[i][j]=num;
+                        r[i]|=(1<<num);
+                        c[j]|=(1<<num);
+                        box[((i/3)*3)+(j/3)]|=(1<<num);
+                        call= call || sudoku(arr, index+1, r, c, box, nos-1);
+                        arr[i][j]=0;
+                        r[i]&=(~(1<<num));
+                        c[j]&=(~(1<<num));
+                        box[((i/3)*3)+(j/3)]&=(~(1<<num));
+                    }
+                }
+            // }
+        // }
+        return call;
+    }
+
+    public static void printArr(int[][]arr){
+        for(int []ele1:arr){
+            for(int ele: ele1){
+                System.out.print(ele+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+    }
+    public static boolean sudokuSafeToPlace(int[][]arr, int num, int row, int col, int[]r, int[]c, int []box){
+        int mask=(1<<num);
+        if(arr[row][col]==0 && ((r[row]& mask)==0) && ((c[col]& mask)==0) && ((box[((row/3)*3)+(col/3)] & mask)==0))
+        return true;
+        return false;
+    }
+
 
     public static void main(String[]args){
         int queens= 4;
-        int[][]arr=new int[10][10];
+        // int[][]arr=new int[10][10];
         // int ans= nQueenBasic(arr, queens, "", 0);
-        int []r= new int[arr[0].length];
-        int []c= new int[arr.length];
-        int []d= new int[arr.length + arr[0].length -1];
-        int []ad= new int[arr.length + arr[0].length -1];
+        // int []r= new int[arr[0].length];
+        // int []c= new int[arr.length];
+        // int []d= new int[arr.length + arr[0].length -1];
+        // int []ad= new int[arr.length + arr[0].length -1];
         // int ans= nQueenShadow(arr, queens, "", 0, r, c, d, ad);
         // int ans= nQueenShadowBetter(arr, queens, "", 0, r, c, d, ad);
         // int ans= nQueenShadowBetterSubsequence(arr, queens, "", 0, 0, r, c, d, ad);
-        int ans= nQueenShadowBetterSubsequenceBetter(arr, queens, "", 0, r, c, d, ad);
-        System.out.println(ans);
+        // int ans= nQueenShadowBetterSubsequenceBetter(arr, queens, "", 0, r, c, d, ad);
+        int[][] arr=constructSudoku();
+        int count=0;
+        for(int []ele1:arr){
+            for(int ele: ele1){
+                if(ele==0)
+                count++;
+            }
+        }
+        boolean call= sudoku(arr, 0, new int[9],  new int[9], new int[9], count);
+        // System.out.println(ans);
     }
 }
