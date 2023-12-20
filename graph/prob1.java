c79d186c8f625b77ec8229bf3022635e1b13af1b
Yash Handa <yashanda@amazon.com>
2023-12-20 16:14:34 +0530

    GCC

--- graph/prob1.java
+++ graph/prob1.java
@@ -24,6 +24,7 @@ public class prob1{
         addEdge(1,2,10);
         addEdge(2,1,10);
         addEdge(2,3,40);
+        addEdge(2,5,5);
         addEdge(3,0,10);
         addEdge(3,2,40);
         addEdge(3,4,2);
@@ -31,6 +32,7 @@ public class prob1{
         addEdge(4,5,2);
         addEdge(4,6,3);
         addEdge(5,4,2);
+        addEdge(5,2,5);
         addEdge(5,6,8);
         addEdge(6,5,8);
         addEdge(6,4,3);
@@ -162,24 +164,151 @@ public class prob1{
         }
     }
 
+    public static int hamiltonianPath(int startVertex, String ans, int[]vis, int noOfVis, int originalStartVertex){
+        if(noOfVis==graph.length){
+            if(positionOfVertex(startVertex, originalStartVertex)!=-1){
+                System.out.println("Hamiltonian Cycle: "+ ans);
+                return 1;
+            }
+            else{
+                System.out.println("Hamiltonian Path: "+ ans);
+                return 0;  
+            }
+        }
+
+        int count=0;
+        vis[startVertex]=1;
+        ArrayList<Edge>neighbours= graph[startVertex];
+        for(int i=0; i<neighbours.size(); i++){
+            int singleNeighbour= neighbours.get(i).v;
+            if(vis[singleNeighbour]==0){
+                vis[singleNeighbour]=1;
+                count+= hamiltonianPath(singleNeighbour, ans+""+singleNeighbour, vis, noOfVis+1, originalStartVertex);
+                vis[singleNeighbour]=0;
+            }
+        }
+        return count;
+    }
+
+    public static void surroundedRegions(char[][] board) {
+       ArrayList<Integer> startPoints = new ArrayList<>();
+        for(int col=0; col<board[0].length; col++){
+            if(board[0][col]=='O')
+            startPoints.add(col);
+            if(board[board.length-1][col]=='O')
+            startPoints.add((board.length-1)*board[0].length +col);
+        }
+        for(int row=0; row<board.length; row++){
+            if(board[row][0]=='O')
+            startPoints.add(board[0].length * row);
+            if(board[row][board[0].length-1]=='O')
+            startPoints.add(board[0].length * row + board[0].length-1);
+        }
+        System.out.println("staret points are: "+startPoints);
+        int[][]dir=new int[4][2];
+        dir[0]=new int[]{0,-1};
+        dir[1]=new int[]{0,1};
+        dir[2]=new int[]{-1,0};
+        dir[3]=new int[]{1,0};
+        for(Integer value: startPoints){
+            int row= value/board[0].length;
+            int col= value%board[0].length;
+            if(board[row][col]!='A')
+            surroundedRegionsDfs(board, value, new int[board.length][board[0].length], dir);  //returns a board with unsurrounded from all sides 0 turned to 'A';
+            print2DArray(board);
+            // break;
+        }
+         for(int row=0; row<board.length; row++){
+            for(int col=0; col<board[0].length; col++){
+                if(board[row][col]=='O')
+                board[row][col]='X';
+                else if(board[row][col]=='A')
+                board[row][col]='O';
+            }
+        }
+    }
+    public static void surroundedRegionsDfs(char[][]board, int index, int[][]vis, int[][]dir){
+       int row= index/board[0].length;
+        int col= index%board[0].length;
+
+        System.out.println("row: "+row + "   col: "+col);
+        vis[row][col]=1;
+        board[row][col]='A';
+        for(int i=0;i<4;i++){
+            int nrow=row+dir[i][0];
+            int ncol=col+dir[i][1];
+            if(nrow>=0 && nrow<board.length && ncol>=0 && ncol<board[0].length && board[nrow][ncol]=='O'){
+                int nindex= nrow*board.length + ncol;
+                if(vis[nrow][ncol]==0){
+                    vis[nrow][ncol]=1;
+                    surroundedRegionsDfs(board, nindex, vis, dir);
+                    vis[nrow][ncol]=0;
+                }
+            }
+        }
+    }
+    public static void print2DArray(char[][]board){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int getConnectedComponents(){
+        int[]vis= new int[graph.length];
+        int count=0;
+        for(int vertex=0; vertex<graph.length; vertex++){
+            if(vis[vertex]==0){
+                count++;
+                GCC(vertex, vis);
+            }
+        }
+        return count;
+    }
+    public static void GCC(int vertex, int[]vis){
+        vis[vertex]=1;
+
+        ArrayList<Edge> neighbours= graph[vertex];
+        for(Edge e: neighbours){
+            int neighbour= e.v;
+            if(vis[neighbour]==0){
+                GCC(neighbour, vis);
+            }
+        }
+    }
+
 
     public static void main(String[]args){
         constructGraph();
-        // removeEdge(1,2);
+        removeEdge(2,5);
+        removeEdge(3,4);
         // removeVertex(3);
-        displayGraph();
-        // boolean res= hasPath(0,6,new boolean[graph.length], "0");
-        // int res= allPath(0,6,new boolean[graph.length], "0", 0);
-        // int ceil=1000, floor=10000, heaviest=0, lightest=1000;
-        display values= new display();
-        int res= allSolution(0,6,new boolean[graph.length], "0", 0, values, 20);
-        System.out.println(res);
+        // displayGraph(); 
+        // display values= new display();
+        // int res= allSolution(0,6,new boolean[graph.length], "0", 0, values, 20);
+        // System.out.println(res);
         //Remember:
         // Primitive data type(int) like heaviest, lightest, etc. cannot be used as variable because their value is passed by value in java always.
         // this holds true for primitive data types
         //whereas for non-primitive data types, values are passed by reference.
-        System.out.println("Ceiling: "+values.ceil + ",        Floor:"+ values.floor);
-        System.out.println("Heaviest: "+ values.heaviest + ",        Lightest: "+ values.lightest);
-
+        // System.out.println("Ceiling: "+values.ceil + ",        Floor:"+ values.floor);
+        // System.out.println("Heaviest: "+ values.heaviest + ",        Lightest: "+ values.lightest);
+        // int res= hamiltonianPath(2, "2", new int[graph.length], 1, 2);
+        // System.out.println(res);
+        char[][]board= new char[4][2];
+        board[0]=new char[]{'X','X','X','X'};
+        board[1]=new char[]{'X','0','0','X'};
+        board[2]=new char[]{'X','0','0','X'};
+        board[3]=new char[]{'X','0','X','X'};
+        // surroundedRegions(board);
+        // board[0]=new char[]{'X','X'};
+        // board[1]=new char[]{'X','X'};
+        // board[2]=new char[]{'X','X'};
+        // board[3]=new char[]{'X','X'};
+        int res= getConnectedComponents();
+        System.out.println(res);
+        // print2DArray(board);
     }
 }
