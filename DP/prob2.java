b843def4deb1ba01c47029104dd4e6f9b3a2952a
Yash Handa <yashpersonal@Yashs-MacBook-Air.local>
2024-02-21 09:52:41 +0530

    extras

--- DP/prob2.java
+++ DP/prob2.java
@@ -2,6 +2,18 @@ import java.io.*;
 
 public class prob2{
     public static void main(String[]args){
-        System.out.println("hello");
+        System.out.println("                       abcde");
+        for(int i=1;i<24;i*=2){
+            for(int j=12;j>=i;j/=2){
+                System.out.print("     ");
+            }
+            for(int j=1;j<=i;j*=2){
+                System.out.print("abcde ");
+            }
+            for(int j=1;j<=i;j*=2){
+                System.out.print("abcde ");
+            }
+            System.out.println();
+        }
     }
 }
