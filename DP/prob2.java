48c660cbd7ddf2999554eefa079f17e90de4d680
Yash Handa <yashhanda500@gmail.com>
2024-02-21 10:01:19 +0530

    test

--- DP/prob2.java
+++ DP/prob2.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class prob2{
     public static void main(String[]args){
-        System.out.println("                       abcde");
+        System.out.println("                       abcde"); 
         for(int i=1;i<24;i*=2){
             for(int j=12;j>=i;j/=2){
                 System.out.print("     ");
