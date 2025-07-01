591e44c7ab63a81829a703c617e4fbfc707d6e85
Yash Handa <yashhanda500@gmail.com>
2025-07-01 23:21:05 +0530

    adding all files

---
+++ out/production/prep/interview1/prob2.js
@@ -0,0 +1,10 @@
+const reverse = string => { 
+    // write code here 
+
+    var temp= //getting string here;
+
+    
+    } 
+    
+    console.log(reverse(" Test user is John   Doe  ") == "Doe John is user Test")
+    console.log(reverse("Jack    &   Jill went up the hill") == 'hill the up went Jill & Jack')
