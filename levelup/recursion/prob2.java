0d8fe93b15e7ea4b3726bb90db816503eef58bfc
yash.handa <yash.handa@kotak.com>
2025-07-08 22:06:09 +0530

    sudoku & print functions

--- levelup/recursion/prob2.java
+++ levelup/recursion/prob2.java
@@ -122,15 +122,137 @@ public class prob2{
         return count;
     }
 
+    public static boolean sudokuCheck(int[][]arr, int idx, int []row, int[]col, int[]box, int no){
+        int n = arr.length;
+        int m = arr[0].length;
+
+        int r = idx/n;
+        int c = idx%n;
+        // System.out.println("coming: "+ idx +" "+no);
+        // printSudoku(arr);
+        int mask = (1<<no);
+        // System.out.println("verify");
+        // System.out.println((row[r] & mask)+ " "+ (col[c] & mask) +" "+ (box[((r/3)*3 + (c/3))] & mask));
+
+        if(((row[r] & mask) == 0) && ((col[c] & mask) == 0) && ((box[((r/3)*3 + (c/3))] & mask) == 0)){
+            // System.out.println("true returned: "+ idx);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int sudoku(int[][]arr, int idx, int []row, int[]col, int[]box){
+        int n = arr.length;
+        int m = arr[0].length;
+        if(idx == n*m){
+            printSudoku(arr);
+            return 1;
+        }
+
+        int count = 0;
+        
+            int r = idx/n;
+            int c = idx%n;
+            // System.out.println("checking2: "+ idx);
+            if(arr[r][c]!=0){
+                count += sudoku(arr, idx+1, row, col, box);
+            }
+            else {
+                for(int j=1; j<=9; j++){
+                    // System.out.println("checking2: "+ idx +" "+ j);
+                    if(sudokuCheck(arr, idx, row, col, box, j)){
+                        // System.out.println("checking: "+ idx +" "+ j);
+                        int mask = (1<<j);
+                        row[r] = row[r]^mask;
+                        col[c] = col[c]^mask;
+                        box[((r/3)*3 + (c/3))] = box[((r/3)*3 + (c/3))]^mask;
+                        arr[r][c] = j;
+                        count += sudoku(arr, idx+1, row, col, box);
+                        arr[r][c] = 0;
+                        row[r] = row[r]^mask;
+                        col[c] = col[c]^mask;
+                        box[((r/3)*3 + (c/3))] = box[((r/3)*3 + (c/3))]^mask;
+                    }
+                }
+            }
+
+        return count;
+    }
+
+    public static int[][] constructSudoku(){
+        int[][]arr={{8,0,0,0,0,0,0,0,0},
+                    {0,0,3,6,0,0,0,0,0},
+                    {0,7,0,0,9,0,2,0,0},
+                    {0,5,0,0,0,7,0,0,0},
+                    {0,0,0,0,4,5,7,0,0},
+                    {0,0,0,1,0,0,0,3,0},
+                    {0,0,1,0,0,0,0,6,8},
+                    {0,0,8,5,0,0,0,1,0},
+                    {0,9,0,0,0,0,4,0,0}};
+        
+        return arr;
+    }
+
+    public static int getEmptyBoxCount(int[][]arr){
+        int count = 0;
+        for(int i=0; i<arr.length; i++){
+            for (int j=0; j<arr[0].length; j++){
+                if(arr[i][j]==0)
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void print1DArray(int[]arr){
+        for(int i=0; i<arr.length; i++)
+        System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+    public static void setInitialForSudoku(int []row, int[]col, int[]box, int [][]arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                int val = arr[i][j];
+                if(val!=0){
+                    row[i] = row[i]^(1<<val);
+                    col[j] = col[j]^(1<<val);
+                    box[(i/3)*3 + (j/3)] = box[(i/3)*3 + (j/3)]^(1<<val);
+                }
+            }
+        }
+    } 
+
+    public static void printSudoku(int[][]arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[]args){
         int r=4,c=4;
-        int[][]arr = new int[r][c];
+        // int[][]arr = new int[r][c];
         // int []row = new int[r];
         // int []col = new int[c];
         // int []diag = new int[r+c-1];
         // int []adiag = new int[r+c-1];
         // nQueen(4, arr, "", 0, 0, row, col, diag, adiag);
         // nQueenBits(4, arr, "", 0, 0, 0, 0, 0, 0);
-        nQueenBitsSubSeq(4, arr, "", 0, 0, 0, 0, 0, 0);
+        // nQueenBitsSubSeq(4, arr, "", 0, 0, 0, 0, 0, 0);
+        int []row = new int[10];
+        int []col = new int[10];
+        int []box = new int[10];
+
+        int[][]sudokuArray = constructSudoku();
+        setInitialForSudoku(row, col, box, sudokuArray);
+        // printSudoku(sudokuArray);
+        // print1DArray(row);
+        // print1DArray(col);
+        // print1DArray(box);
+
+        System.out.println(sudoku(sudokuArray, 0, row, col, box));
     }
 }
